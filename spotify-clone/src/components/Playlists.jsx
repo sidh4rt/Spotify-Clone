ba9245ea92d58877a52c5d6b142141e665ab575b
Siddharth <sid.jangpangi@gmail.com>
2022-09-19 19:21:59 +0530

    added scroll bar in playlists

--- spotify-clone/src/components/Playlists.jsx
+++ spotify-clone/src/components/Playlists.jsx
@@ -37,12 +37,23 @@ export default function Playlists() {
 }
 
 const Container = styled.div`
-ul {
+  height: 100%;
+  overflow: hidden;
+  ul {
     list-style-type: none;
     display: flex;
     flex-direction: column;
     gap: 1rem;
     padding: 1rem;
+    height: 52vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
     li {
       display: flex;
       gap: 1rem;
